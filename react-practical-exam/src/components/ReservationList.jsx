9591f64b0bfc71dc36740e3811b75623fddd9333
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2026-01-28 12:40:54 +0530

    Add files via upload

--- react-practical-exam/src/components/ReservationList.jsx
+++ react-practical-exam/src/components/ReservationList.jsx
@@ -15,7 +15,7 @@ export default function ReservationList() {
       <h3 style={{color:"white"}}>Reservations</h3>
       {list.map(r => (
         <div key={r.id} className="card p-3"  style={{backgroundColor:" rgb(35, 66, 56)", color:"white", fontSize:"18px", fontWeight:"bold"}}>
-          Room: {r.roomId}
+          Room:  {r.roomId}  | {r.status === "id" ? "Available" : "Booked"}
           <button
             className="btn btn-danger btn-sm float-end"
             onClick={() => dispatch(cancelReservation(r.id))}
